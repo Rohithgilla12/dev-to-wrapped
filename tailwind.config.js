5683408fa5c44e474bbec3e4f6abc0f23daf2a07
rohithgilla <gillarohith1@gmail.com>
2021-12-26 01:05:31 +0530

    Init Remix + Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fromRoot = (p) => path.join(__dirname, p);
+
+module.exports = {
+  // the NODE_ENV thing is for https://github.com/Acidic9/prettier-plugin-tailwind/issues/29
+  mode: process.env.NODE_ENV ? "jit" : undefined,
+  content: ["./app/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  purge: {
+    mode: "layers",
+    enabled: process.env.NODE_ENV === "production",
+    content: [fromRoot("./app/**/*.+(js|ts|tsx|mdx|md)")],
+  },
+  plugins: [],
+};
